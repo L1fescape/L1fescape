a6865f046e0295ba4a138577d21931996c55d50e
Andrew Kennedy <akennedy2292@gmail.com>
2018-06-06 01:22:17 -0700

    upgrade to webpack 4
    implement codesplitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const fs = require('fs')
 
 const src = path.resolve(__dirname, 'src')
+const pages = path.resolve(src, 'pages')
 const htdocs = path.resolve(__dirname, 'htdocs')
 const modules = path.resolve(__dirname, 'node_modules')
 const dist = path.resolve(__dirname, 'dist')
 
+const pageEntriesReducer = (acc, key) => {
+  acc[key] = `${path.resolve(pages, key)}/index.ts`
+  return acc
+}
+const pageEntries = fs.readdirSync(pages).reduce(pageEntriesReducer, {})
+
 module.exports = {
-  entry: './src/index.tsx',
+  entry: {
+    index: './src/index.tsx',
+    ...pageEntries,
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: dist
   },
   resolve: {
@@ -36,11 +47,16 @@ module.exports = {
   ],
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
       { test: /\.js$/, enforce: 'pre', loader: 'source-map-loader' },
       { test: /\.scss$/, use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }] },
       { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader?name=[name].[ext]' },
       { test: /\.md$/, use: [{ loader: 'html-loader' }] },
     ],
-  }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+  },
 }
