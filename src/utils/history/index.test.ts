a29bf5d2bd746ef63d53149a151f16eaea003cb3
L1fescape <akennedy2292@gmail.com>
2019-04-10 17:26:03 -0700

    unbreak tests

--- src/utils/history/index.test.ts
+++ src/utils/history/index.test.ts
@@ -20,7 +20,7 @@ test('preserves scroll position of previous pages when navigating back', () => {
 
   // navigate to page 1 and scroll down
   onRouteChange(history[0], 'PUSH')
-  expect(setScrollPos).toHaveBeenCalledWith(1000)
+  expect(setScrollPos).toHaveBeenCalledWith(0)
   scrollPos = 100
   // navigate to page 2 and scroll down again
   onRouteChange(history[1], 'PUSH')
