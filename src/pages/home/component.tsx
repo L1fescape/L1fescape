d3d4ce612f438e5b3a0fd4c5f25704291dd3ac02
Andrew Kennedy <akennedy2292@gmail.com>
2019-10-29 04:40:53 +0000

    hide unfinished sections and pages for now

--- src/pages/home/component.tsx
+++ src/pages/home/component.tsx
@@ -7,7 +7,7 @@ import { Socials, Links } from 'ak.gg/components/socials'
 import './styles.scss'
 
 export const Home = () => (
-  <Page className="home" hideHomeLink pageSource={__filename}>
+  <Page className="home" noHeader noFooter pageSource={__filename}>
     <Woulg className="hi">
       <section>
         <div className="intro">
@@ -19,32 +19,5 @@ export const Home = () => (
         </div>
       </section>
     </Woulg>
-    <section className="about">
-      <h2>Hello! 👋</h2>
-      <h3>Quick Intro</h3>
-      <p>
-        {`I'm Andrew and I like writing music, skateboarding, and making things
-        with code. During the day I'm a Software Engineer at `}
-        <Links.SauceLabs />. At night I work on{' '}
-        <Links.GitHub>open source projects</Links.GitHub> and make beats.
-      </p>
-    </section>
-    <section className="projects">
-      <h2>Projects</h2>
-      <div className="content">
-        <div className="project">
-          <h3>Tetris</h3>
-        </div>
-        <div className="project">
-          <h3>Life</h3>
-          <h4>Magic: The Gathering Life Counter</h4>
-        </div>
-      </div>
-    </section>
-    <section className="blog">
-      <h2>
-        Latest from the <Link to={Routes.Blog}>Blog</Link>
-      </h2>
-    </section>
   </Page>
 )
