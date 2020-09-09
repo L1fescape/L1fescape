a1bbf43019175482903b9f5458e60b2fc493779e
Andrew Kennedy <akennedy2292@gmail.com>
2020-09-09 01:55:18 -0700

    add new playlist id format w/ tests

---
+++ src/components/spotify/playlist/api.test.ts
@@ -0,0 +1,15 @@
+import { parsePlaylistId } from './api'
+
+test('parses spotify playlist ids', () => {
+  expect(parsePlaylistId('1IoSlQbiD7OZ7APX3tviuX')).toBe(
+    '1IoSlQbiD7OZ7APX3tviuX'
+  )
+  expect(parsePlaylistId('spotify:playlist:1IoSlQbiD7OZ7APX3tviuX')).toBe(
+    '1IoSlQbiD7OZ7APX3tviuX'
+  )
+  expect(
+    parsePlaylistId(
+      'https://open.spotify.com/playlist/1IoSlQbiD7OZ7APX3tviuX?si=1uNY_xZFQxK8iOXeIYlLfg'
+    )
+  ).toBe('1IoSlQbiD7OZ7APX3tviuX')
+})
--- src/components/spotify/playlist/api.ts
+++ src/components/spotify/playlist/api.ts
@@ -4,6 +4,7 @@ export const playlistIDs: string[] = [
   'spotify:playlist:1IoSlQbiD7OZ7APX3tviuX',
   'spotify:playlist:0Qf5El4jvjpUnsJ4YZdTKo',
   'spotify:playlist:1EOj4DNc4LJD6ujTkFu6N7',
+  'https://open.spotify.com/playlist/19x8zn8rBoAzl0EEciH14w?si=1uNY_xZFQxK8iOXeIYlLfg',
 ]
 
 export interface PlaylistData {
@@ -20,13 +21,16 @@ export interface PlaylistData {
 
 const base = 'https://api.spotify.com/v1'
 
-function formatPlaylistId(id: string): string {
+export function parsePlaylistId(id: string): string {
   const parts = id.split(':')
   if (parts.length === 1) {
     return id
   }
   if (parts.length === 3 && parts[1] === 'playlist') {
     return parts[2]
+  }
+  if (id.indexOf('://') > -1) {
+    return id.substring(id.lastIndexOf('/') + 1, id.indexOf('?'))
   }
   throw new Error(`unknown id: ${id}`)
 }
@@ -35,7 +39,8 @@ export async function getPlaylist(
   token: string,
   id: string
 ): Promise<PlaylistData> {
-  id = formatPlaylistId(id)
+  id = parsePlaylistId(id)
+  console.log(id)
   try {
     const response = await fetch(`${base}/playlists/${id}`, {
       method: 'GET',
