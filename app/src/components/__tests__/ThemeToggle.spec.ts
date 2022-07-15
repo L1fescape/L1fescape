b408f76566086c61a2d61fb5fab04390446af560
Andrew Kennedy <akennedy2292@gmail.com>
2022-07-15 16:09:56 +0200

    remove cypress for now

--- app/src/components/__tests__/ThemeToggle.spec.ts
+++ app/src/components/__tests__/ThemeToggle.spec.ts
@@ -1,23 +1,33 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
 
 import { mount } from '@vue/test-utils';
 import ThemeToggle from '../ThemeToggle.vue';
 
+// mock window.matchMedia
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
-  value: jest.fn().mockImplementation((query) => ({
+  value: vi.fn().mockImplementation((query) => ({
     matches: false,
     media: query,
     onchange: null,
-    addListener: jest.fn(), // Deprecated
-    removeListener: jest.fn(), // Deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
+    addListener: vi.fn(), // Deprecated
+    removeListener: vi.fn(), // Deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
   })),
 });
 
 describe('ThemeToggle', () => {
+  beforeEach(() => {
+    // tell vitest we use mocked time
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
   it('renders properly', () => {
     const wrapper = mount(ThemeToggle);
     expect(wrapper.attributes().title).toEqual('Toggle Theme');
